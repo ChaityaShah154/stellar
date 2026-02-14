da94d2662fc2769790214316e9d4251eeabfadbb
chaityameehu <chaityameehika0122@gmail.com>
2026-02-14 11:47:55 +0530

    new deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/stellar/',
+  base: '/stellar',
   plugins: [
     react({
       babel: {
